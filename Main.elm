5ff45675dd5e3be478eac186b33025e31a3da5f5
Simon Sawert <simon@sawert.se>
2019-06-25 10:01:54 +0200

    ☝🏼 Elm 0.19

--- Main.elm
+++ Main.elm
@@ -1,18 +1,23 @@
-module Main exposing (..)
+module Main exposing (Language, Model, Msg(..), accessLanguage, addLanguage, flagLink, init, languages, main, splashImage, update, view)
 
+import Browser
+import Browser.Navigation as Nav
 import Dict exposing (get)
-import Html exposing (Html, a, div, figure, figcaption, img, strong, span, text)
+import Html exposing (Html, a, div, figcaption, figure, img, span, strong, text)
 import Html.Attributes exposing (alt, href, id, src)
-import Navigation
-import String.Extra exposing (rightOf)
+import String.Extra exposing (clean, rightOf)
+import Url
 
 
+main : Program () Model Msg
 main =
-    Navigation.program UrlChange
+    Browser.application
         { init = init
         , view = view
         , update = update
-        , subscriptions = (\_ -> Sub.none)
+        , subscriptions = \_ -> Sub.none
+        , onUrlChange = UrlChanged
+        , onUrlRequest = LinkClicked
         }
 
 
@@ -47,32 +52,66 @@ languages =
         [ ( "se"
           , addLanguage
                 "Det blev ingen cd."
-                "Några 15-åringar från Domarhagsskolan tänkte kolla in cd-skivor under lunchrasten. Men affären de gick till hade stängt. Från vänster står Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson och Max Malmström."
+                ("""Några 15-åringar från Domarhagsskolan tänkte kolla in
+                cd-skivor under lunchrasten. Men affären de gick till hade
+                stängt. Från vänster står Alfredo Pettersson, Richard Knöös,
+                Tommy Carlberg, Mattias Persson och Max Malmström."""
+                    |> clean
+                )
           )
         , ( "dk"
           , addLanguage
                 "Der blev ingen cd."
-                "Nogle 15-årige drenge fra Domarhagsskolen tænkte at se på nogle cd'er i frokostpausen. Men butikken de gik til var lukket. Fra venstre står Alfredo Pettersson, Richard Knöos, Tommy Carlberg, Mattias Persson og Max Malmström."
+                ("""Nogle 15-årige drenge fra Domarhagsskolen tænkte at se på
+                nogle cd'er i frokostpausen. Men butikken de gik til var lukket.
+                Fra venstre står Alfredo Pettersson, Richard Knöos, Tommy
+                Carlberg, Mattias Persson og Max Malmström."""
+                    |> clean
+                )
           )
         , ( "us"
           , addLanguage
                 "They didn't get any cd."
-                "A few 15 year old boys from Domarhagsskolan were going to check out CD's during the lunch break. But the store they went to was closed. Standing from the left; Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson and Max Malmström."
+                ("""A few 15 year old boys from Domarhagsskolan were going to
+                check out CD's during the lunch break. But the store they went
+                to was closed. Standing from the left; Alfredo Pettersson,
+                Richard Knöös, Tommy Carlberg, Mattias Persson and Max
+                Malmström."""
+                    |> clean
+                )
           )
         , ( "de"
           , addLanguage
                 "Sie erhielten kein cd."
-                "Einige 15 jahre alte jungen von Domarhagsskolan waren im begriff, aus CD's während der mittagspause zu überprüfen. Aber der speicher gingen sie zu waren geschlossen. Stehen vom links; Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson und Max Malmström."
+                ("""Einige 15 jahre alte jungen von Domarhagsskolan waren im
+                begriff, aus CD's während der mittagspause zu überprüfen. Aber
+                der speicher gingen sie zu waren geschlossen. Stehen vom links;
+                Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias
+                Persson und Max Malmström."""
+                    |> clean
+                )
           )
         , ( "fr"
           , addLanguage
                 "Ils n'ont obtenu aucun cd."
-                "Quelques 15 années de garçons de Domarhagsskolan allaient vérifier CD's pendant la pause de midi. Mais le magasin elles sont allées à étaient fermées. Se tenir de la gauche ; Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson, Max Malmström."
+                ("""Quelques 15 années de garçons de Domarhagsskolan allaient
+                vérifier CD's pendant la pause de midi. Mais le magasin elles
+                sont allées à étaient fermées. Se tenir de la gauche ; Alfredo
+                Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson, Max
+                Malmström."""
+                    |> clean
+                )
           )
         , ( "es"
           , addLanguage
                 "No consiguieron ningún cd."
-                "Algunos 15 años de viejos muchachos de Domarhagsskolan iban a comprobar fuera de CD's durante la hora de la almuerzo. Pero el almacén fueron a eran cerrados. El estar parado de la izquierda; Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias Persson y Max Malmström."
+                ("""Algunos 15 años de viejos muchachos de Domarhagsskolan iban a
+                comprobar fuera de CD's durante la hora de la almuerzo. Pero el
+                almacén fueron a eran cerrados. El estar parado de la izquierda;
+                Alfredo Pettersson, Richard Knöös, Tommy Carlberg, Mattias
+                Persson y Max Malmström."""
+                    |> clean
+                )
           )
         ]
 
@@ -82,17 +121,15 @@ languages =
 
 
 type alias Model =
-    { lang : String }
+    { lang : String
+    , key : Nav.Key
+    , url : Url.Url
+    }
 
 
-model : Model
-model =
-    { lang = "" }
-
-
-init : Navigation.Location -> ( Model, Cmd Msg )
-init location =
-    ( Model (rightOf "#" location.hash), Cmd.none )
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init flags url key =
+    ( Model (rightOf "#" (Url.toString url)) key url, Cmd.none )
 
 
 
@@ -100,28 +137,39 @@ init location =
 
 
 type Msg
-    = UrlChange Navigation.Location
+    = LinkClicked Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
-update : Msg -> Model -> ( Model, Cmd msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        UrlChange location ->
-            ( { model | lang = rightOf "#" location.hash }, Cmd.none )
+        LinkClicked urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Nav.load href )
+
+        UrlChanged url ->
+            ( { model | lang = rightOf "#" (Url.toString url), url = url }, Cmd.none )
 
 
 
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
     let
         currentLang =
             Dict.get model.lang languages
                 |> accessLanguage
     in
-        div [ id "wrapper" ]
+    { title = currentLang.lead
+    , body =
+        [ div [ id "wrapper" ]
             [ a [ href "https://github.com/bombsimon/det-blev-ingen-cd.com" ]
                 [ img
                     [ id "github-ribbon"
@@ -147,6 +195,8 @@ view model =
                     ]
                 ]
             ]
+        ]
+    }
 
 
 splashImage : String -> Html Msg
